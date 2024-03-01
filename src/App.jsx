2c48eb9274d533a5b65a6b343c718a909186fa4a
Manu S <manus@leuwint.com>
2024-03-01 21:40:13 +0530

    base_url

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   const fetchImages = async () => {
     try {
-      const response = await fetch("http://localhost:3000/images");
+      const response = await fetch(`${BASE_URL}images`);
       if (!response.ok) {
         throw new Error("Failed to fetch images");
       }
