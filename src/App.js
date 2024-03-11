e8838ec5b989d1b8bba1c02534e305087e67a801
Manu S <ms.manu.856@gmail.com>
2024-03-11 15:13:10 +0530

    routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import './components/dist/style.css'
+import './components/dist/style.css';
 import Splash from './components/pages/Splash';
 import Gallery from './components/pages/Gallery';
-import { AnimatePresence } from 'framer-motion';
-
+import Admin from './components/pages/admin/Admin';
+import ImageGallery from './components/pages/admin/ImageGallery';
 
 function App() {
   return (
-    <>
-      {/* <Splash />
-      <Gallery /> */}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/image" element={<ImageGallery />} />
+      </Routes>
+    </Router>
   );
 }
 
