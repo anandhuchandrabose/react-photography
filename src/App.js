31c220de9be0daec8b84f82774e5174c99e43de2
anandhuchandrabose <anandhuchandrabose2000@gmail.com>
2024-05-04 02:55:11 +0530

    publish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ComingSoon from './test/comingsoon';
+// import ComingSoon from './test/comingsoon';
+import LinkTree from './LinkTree/LinkTree';
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         {/* COMING SOON */}
-        <Route path="/" element={<ComingSoon />} />
+        <Route path="/" element={<LinkTree />} />
       </Routes>
     </Router>
   );
