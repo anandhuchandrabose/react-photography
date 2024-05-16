1229ce8c2b25f79789c507c0d1ac4eac0e3a84a8
manu-git-hub <ms.manu.856@gmail.com>
2024-05-16 16:23:26 +0530

    bodyparser

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const getImageRoutes = require('./routes/getImageRouter');
 const uploadImageRoutes = require('./routes/uploadImageRouter');
 const deleteImageRoutes = require('./routes/deleteImageRouter');
@@ -9,7 +10,16 @@ const app = express();
 
 // Enable CORS for all requests
 app.use(cors());
+const corsOptions = {
+  origin: 'https://storiesofragooty.com' // Allow requests from this domain
+};
 
+// Enable CORS for specific domain
+app.use(cors(corsOptions));
+
+// Increase payload size limit
+app.use(bodyParser.json({ limit: '500mb' }));
+app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
 // Middleware to parse JSON bodies
 app.use(express.json());
 
